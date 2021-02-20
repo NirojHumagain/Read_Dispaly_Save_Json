b5a009efec9f9eaf15daa3d5d8dfec26dd601476
Niroj Humagain <nirojhumagain8488@icloud.com>
2021-02-20 16:36:28 +0545

    C# Project

---
+++ Read_Display_Save_Json/Pages/Index.cshtml.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using QuickType;
+using System.Data.SqlClient;
+using System.Text;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace WebApplication1.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+            using (var webclient = new WebClient())
+
+            {
+                var filePath = @"C:\Users\Niroj Humagain\Desktop\MOCK_DATA.json";
+                bool r =File.Exists(filePath);
+
+                if (r)
+                {
+                    var fileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read);
+                    using (var streamReader = new StreamReader(fileStream, Encoding.UTF8))
+                    {
+                       var json = streamReader.ReadToEnd();
+                        var welcome = Welcome.FromJson(json);
+                        using (SqlConnection connection = new SqlConnection())
+                        {
+                            connection.ConnectionString = "Server=localhost\\MSSQLSERVER01;Database=SqlDataSave;Trusted_Connection=True;";
+                            connection.Open();
+                           
+                            foreach (var item in welcome)
+                            {
+                                SqlCommand insertCommand = new SqlCommand("INSERT INTO Welcome (Id,First_Name, Last_Name, Email, Gender,Ip_Address) VALUES (@id, @fname, @lname, @email, @gender, @ip)", connection);
+
+                                insertCommand.Parameters.Add(new SqlParameter("@id", item.Id));
+                                insertCommand.Parameters.Add(new SqlParameter("@fname", item.First_Name));
+                                insertCommand.Parameters.Add(new SqlParameter("@lname", item.Last_Name));
+                                insertCommand.Parameters.Add(new SqlParameter("@email", item.Email));
+                                insertCommand.Parameters.Add(new SqlParameter("@gender", item.Gender));
+                                insertCommand.Parameters.Add(new SqlParameter("@ip", item.Ip_Address));
+                                insertCommand.ExecuteNonQuery();
+                            }
+                            ViewData["Welcome"] = json;
+                        }
+                        //}
+                    }
+                }
+                else {
+                    ViewData["ErrorMessage"] = "File does not exists";
+
+                }
+                ////string jsonString = webclient.DownloadString(@"C:\Users\Niroj Humagain\Desktop\New folder (2)\WebApplication1\WebApplication1\MOCK_DATA.json");
+                //if(string.IsNullOrEmpty(jsonString)) {
+                //    return ; 
+                //}
+                //else
+                //{
+                //    var welcome = Welcome.FromJson(jsonString);
+
+                //    using (SqlConnection connection = new SqlConnection())
+                //    {
+                //        connection.ConnectionString = "Server=localhost\\MSSQLSERVER01;Database=SqlDataSave;Trusted_Connection=True;";
+                //        connection.Open();
+                //        //insertCommand.Parameters.Add(new SqlParameter("@0", 1));
+                //        //insertCommand.Parameters.Add(new SqlParameter("@1", "Nikesh"));
+                //        //insertCommand.Parameters.Add(new SqlParameter("@2", "Niraula"));
+                //        //insertCommand.Parameters.Add(new SqlParameter("@3", "aa"));
+                //        //insertCommand.Parameters.Add(new SqlParameter("@4", "aa"));
+                //        //insertCommand.Parameters.Add(new SqlParameter("@5", "aaaa"));
+                //        //insertCommand.ExecuteNonQuery();
+                //        foreach (var item in welcome)
+                //        {
+                //            SqlCommand insertCommand = new SqlCommand("INSERT INTO Welcome (Id,First_Name, Last_Name, Email, Gender,Ip_Address) VALUES (@id, @fname, @lname, @email, @gender, @ip)", connection);
+
+                //            insertCommand.Parameters.Add(new SqlParameter("@id", item.Id));
+                //            insertCommand.Parameters.Add(new SqlParameter("@fname", item.First_Name));
+                //            insertCommand.Parameters.Add(new SqlParameter("@lname", item.Last_Name));
+                //            insertCommand.Parameters.Add(new SqlParameter("@email", item.Email));
+                //            insertCommand.Parameters.Add(new SqlParameter("@gender", item.Gender));
+                //            insertCommand.Parameters.Add(new SqlParameter("@ip", item.Ip_Address));
+                //            insertCommand.ExecuteNonQuery();
+                //        }
+                //        ViewData["Welcome"] = welcome;
+                //    }
+                //}
+
+            }
+
+        }
+    }
+}   
+
+
---
+++ Read_Display_Save_Json/SQliteDbAccess.cs
@@ -0,0 +1,45 @@
+﻿//using system;
+//using system.collections.generic;
+//using system.configuration;
+//using system.data;
+//using system.linq;
+//using system.threading.tasks;
+//using dapper;
+//using microsoft.data.sqlite;
+//using quicktype;
+
+//namespace webapplication1
+//{
+    //public class SQliteDbAccess
+    //{
+    //    public static persondeatil loadperson()
+    //    {
+    //        using (idbconnection cnn = new sqliteconnection(loadconnectionstring()))
+    //        {
+    //            var output = cnn.query("select * from persondetail", dynamicparameters());
+    //            //return output.toarray();
+
+    //        }
+    //    }
+
+    //    private static object dynamicparameters()
+    //    {
+    //        throw new notimplementedexception();
+    //    }
+
+    //    public static void saveperson(persondeatil person)
+    //    {
+    //        using (idbconnection cnn = new sqliteconnection(loadconnectionstring()))
+    //        {
+
+    //        }
+    //    }
+
+
+    //    private static string loadconnectionstring(string id = "default")
+    //    {
+    //        return configurationmanager.connectionstrings[id].connectionstring;
+    //    }
+
+    //}
+
